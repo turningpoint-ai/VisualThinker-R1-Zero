b7b651fb78391d6f81b33451cb7f1c152da5e52e
Ikko Eltociear Ashimine <eltociear@gmail.com>
2025-03-05 03:10:27 +0900

    chore: update setup.sh
    Addtional -> Additional

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ conda activate VisualThinker
 cd src/open-r1-multimodal
 pip install -e ".[dev]"
 
-# Addtional modules
+# Additional modules
 pip install wandb==0.18.3
 pip install tensorboardx
 pip install qwen_vl_utils torchvision
